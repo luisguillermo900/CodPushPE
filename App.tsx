f236db44a1ab78b38c1b6cbea15ce4827cd457d3
luisguillermo900 <lchirinosco@unsa.edu.pe>
2025-07-17 22:38:47 -0500

    add codepush

--- App.tsx
+++ App.tsx
@@ -1,28 +1,58 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
+ * @flow
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import codePush from "react-native-code-push";
 
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
-  );
+export default class codepushSample extends Component {
+
+
+componentDidMount(){
+    var updateDialogOptions = {
+          updateTitle: "Update",
+          optionalUpdateMessage: "New version of the app is available. Install?",
+          optionalIgnoreButtonLabel: "Later",
+          optionalInstallButtonLabel: "Yes",
+        };
+
+    codePush.sync({ updateDialog: updateDialogOptions});
+}
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Initial Version (v2.0)
+        </Text>
+        
+        
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
 });
 
-export default App;
+AppRegistry.registerComponent('codepushSample', () => codepushSample);
